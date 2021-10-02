16efb3688e9aa87122affbfcafe866c6dcb25a7e
程威 <2992112463@qq.com>
2021-10-02 22:43:26 +0800

    test

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '../websokcet-serve.js'
 createApp(App).use(store).use(router).mount('#app')
